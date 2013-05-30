c8f55be89feca71fb94199b8768776c80b52d45f
yang2507366 <yang380065007@hotmail.com>
2013-05-30 20:10:18 +0800

    add project WorkspaceLibs

---
+++ WorkspaceLibs/WorkspaceLibs/WorkspaceLibs.h
@@ -0,0 +1,13 @@
+//
+//  WorkspaceLibs.h
+//  WorkspaceLibs
+//
+//  Created by yangzexin on 5/30/13.
+//  Copyright (c) 2013 yangzexin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WorkspaceLibs : NSObject
+
+@end
---
+++ WorkspaceLibs/WorkspaceLibs/WorkspaceLibs.m
@@ -0,0 +1,13 @@
+//
+//  WorkspaceLibs.m
+//  WorkspaceLibs
+//
+//  Created by yangzexin on 5/30/13.
+//  Copyright (c) 2013 yangzexin. All rights reserved.
+//
+
+#import "WorkspaceLibs.h"
+
+@implementation WorkspaceLibs
+
+@end
